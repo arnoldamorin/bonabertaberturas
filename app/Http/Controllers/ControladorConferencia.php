be0777a4273b4dcf0583d433481320c69cbbb166
ntarche <nelson.tarche@gmail.com>
2020-11-13 20:55:50 -0300

    cambios

--- app/Http/Controllers/ControladorConferencia.php
+++ app/Http/Controllers/ControladorConferencia.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Entidades\Sistema\Patente;
@@ -11,27 +12,8 @@ class ControladorConferencia extends Controller
 {
     public function nuevo()
     {
-        $titulo = "Nueva Conferencia";
-        $entidad = new Conferencia();
-        $array_menu = $entidad->obtenerConferenciaPadre();
-        return view('sistema.conferencia-nuevo', compact('titulo', 'array_menu'));
-
-    }
-}
-namespace App\Http\Controllers;
-
-use App\Entidades\Sistema\Patente;
-use App\Entidades\Sistema\Usuario;
-use Illuminate\Http\Request;
-
-require app_path() . '/start/constants.php';
-
-class ControladorConferencia extends Controller
-{
-    public function nuevo()
-    {
-        $titulo = "Nueva Conferencia";
-        return view('sistema.conferencia-nuevo', compact('titulo', 'array_menu'));
+        $titulo = "Nueva conferencia";
+        return view('conferencia.conferencia-nuevo', compact('titulo'));
 
     }
 }
