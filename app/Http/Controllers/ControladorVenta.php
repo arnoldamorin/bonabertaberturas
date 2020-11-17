c416b53a6c116c2a6edb629c108d14ee9fd2215f
rozapa1998 <rodrigo1998.rz@gmail.com>
2020-11-17 20:07:34 -0300

    cambios

--- app/Http/Controllers/ControladorVenta.php
+++ app/Http/Controllers/ControladorVenta.php
@@ -11,12 +11,80 @@ require app_path() . '/start/constants.php';
 
 class ControladorVenta extends Controller
 {
-    public function nuevo()
+    public function index()
     {
         $titulo = "Nuevo Venta";
-        return view('ventas.venta-listar', compact('titulo','array_menu'));
-
+        if (Usuario::autenticado() == true) {
+            /*if (!Patente::autorizarOperacion("MENUCONSULTA")) {
+                $codigo = "MENUCONSULTA";
+                $mensaje = "No tiene permisos para la operaci&oacute;n.";
+                return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
+            } else {*/
+                return view('venta.venta-listar', compact('titulo'));
+            //}
+        } else {
+            return redirect('admin/login');
+        }
     }
+
+    public function cargarGrilla()
+        {
+            $request = $_REQUEST;
+    
+            $entidadCategoria = new Categoria();
+            $aCategorias = $entidadCategoria->obtenerFiltrado();
+    
+            $data = array();
+    
+            $inicio = $request['start'];
+            $registros_por_pagina = $request['length'];
+    
+            if (count($aCategorias) > 0) {
+                $cont = 0;
+            }
+    
+            for ($i = $inicio; $i < count($aCategorias) && $cont < $registros_por_pagina; $i++) {
+                $row = array();
+                $row[] = $aCategorias[$i]->nombre;
+                $row[] = $aCategorias[$i]->descripcion;                
+                $row[] = "<a href='/admin/cursos/categorias/nuevo/".$aCategorias[$i]->idcategoria."'><i class='fas fa-search'></i></a>";
+                $cont++;
+                $data[] = $row;
+            }
+    
+            $json_data = array(
+                "draw" => intval($request['draw']),
+                "recordsTotal" => count($aCategorias), //cantidad total de registros sin paginar
+                "recordsFiltered" => count($aCategorias), //cantidad total de registros en la paginacion
+                "data" => $data,
+            );
+            return json_encode($json_data);
+        }
+
+        public function nuevo()
+        {
+            $titulo = "Nuevo Categoria";
+            return view('categoria.categoria-nuevo', compact('titulo'));
+        }
+
+        public function editar($id)
+        {
+            $titulo = "Modificar Categoria";
+            if (Usuario::autenticado() == true) {
+                if (!Patente::autorizarOperacion("MENUMODIFICACION")) {
+                    $codigo = "MENUMODIFICACION";
+                    $mensaje = "No tiene pemisos para la operaci&oacute;n.";
+                    return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
+                } else {
+                    $categoria = new Categoria();
+                    $categoria->obtenerPorId($id);
+    
+                    return view('categoria.categoria-nuevo', compact('categoria', 'titulo'));
+                }
+            } else {
+                return redirect('admin/login');
+            }
+        }
 
     public function guardar(Request $request)
         {
@@ -53,7 +121,30 @@ class ControladorVenta extends Controller
                 $msg["MSG"] = ERRORINSERT;
             }
         }
-
-}
+public function eliminar(Request $request)
+        {
+            $id = $request->input('id');
+    
+            if (Usuario::autenticado() == true) {
+                if (Patente::autorizarOperacion("MENUELIMINAR")) {
+                    $entidad = new Categoria();
+                    $entidad->cargarDesdeRequest($request);
+                   
+                    if ($entidad->nombre != "") {
+                        $entidad->eliminar();
+                        $aResultado["err"] = EXIT_SUCCESS; //eliminado correctamente
+                    } else {
+                        $aResultado["err"] = MSG_ERROR;
+                    }    
+                } else {
+                    $codigo = "ELIMINARPROFESIONAL";
+                    $aResultado["err"] = "No tiene pemisos para la operaci&oacute;n.";
+                }
+                echo json_encode($aResultado);
+            } else {
+                return redirect('admin/login');
+            }
+        }
+    }
 
 ?>
