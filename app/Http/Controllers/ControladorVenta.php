85a4dc75f5785aeb985c28762f75cc99845e8106
rozapa1998 <rodrigo1998.rz@gmail.com>
2020-11-17 19:47:37 -0300

    cambios

--- app/Http/Controllers/ControladorVenta.php
+++ app/Http/Controllers/ControladorVenta.php
@@ -17,6 +17,42 @@ class ControladorVenta extends Controller
 
     }
 
+    public function guardar(Request $request)
+        {
+            try {
+                //Define la entidad servicio
+                $titulo = "Modificar Venta";
+                $entidad = new Venta();
+                $entidad->cargarDesdeRequest($request);
+    
+                //validaciones
+                if ($entidad->nombre == "") {
+                    $msg["ESTADO"] = MSG_ERROR;
+                    $msg["MSG"] = "Complete todos los datos";
+                } else {
+                    if ($_POST["id"] > 0) {
+                        //Es actualizacion
+                        $entidad->guardar();
+    
+                        $msg["ESTADO"] = MSG_SUCCESS;
+                        $msg["MSG"] = OKINSERT;
+                    } else {
+                        //Es nuevo
+                        $titulo = "Nuevo Venta";
+                        $entidad->insertar();
+    
+                        $msg["ESTADO"] = MSG_SUCCESS;
+                        $msg["MSG"] = OKINSERT;
+                    }
+                    $_POST["id"] = $entidad->idinscripcion;
+                    return view('venta.venta-nuevo', compact('titulo', 'msg'));
+                }
+            } catch (Exception $e) {
+                $msg["ESTADO"] = MSG_ERROR;
+                $msg["MSG"] = ERRORINSERT;
+            }
+        }
+
 }
 
 ?>
