a4c73327bd6947c5b8475925d242d2d2c4930b60
Diseño Grafico <Disenio@LOCAL.PAVIR.COM.AR>
2020-11-18 12:14:58 -0300

    categorias

--- app/Http/Controllers/ControladorCategoria.php
+++ app/Http/Controllers/ControladorCategoria.php
@@ -45,7 +45,7 @@
     
             for ($i = $inicio; $i < count($aCategorias) && $cont < $registros_por_pagina; $i++) {
                 $row = array();
-                $row[] = $aCategorias[$i]->nombre;
+                $row[] = '<a href="/admin/sistema/menu/' . $aCategorias[$i]->idcategoria . '">' . $aCategorias[$i]->nombre . '</a>';
                 $row[] = $aCategorias[$i]->descripcion;                     
                 $cont++;
                 $data[] = $row;
