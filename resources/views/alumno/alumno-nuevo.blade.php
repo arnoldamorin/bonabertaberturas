28ea7ea43194c717059180162470385543e50ad4
israShort <israshort@gmail.com>
2020-11-20 19:15:39 -0300

    cambiosIconos

--- resources/views/alumno/alumno-nuevo.blade.php
+++ resources/views/alumno/alumno-nuevo.blade.php
@@ -20,7 +20,7 @@
         <li class="btn-item"><a title="Eliminar" href="#" class="fa fa-trash-o" aria-hidden="true" onclick="javascript: $('#mdlEliminar').modal('toggle');"><span>Eliminar</span></a>
         </li>
     @endif
-    <li class="btn-item"><a title="Salir" href="#" class="fa fa-arrow-circle-o-left" aria-hidden="true" onclick="javascript: $('#modalSalir').modal('toggle');"><span>Salir</span></a></li>
+    <li class="btn-item"><a title="Salir" href="#" class="fas fa-reply" aria-hidden="true" onclick="javascript: $('#modalSalir').modal('toggle');"><span>Salir</span></a></li>
 </ol>
 <script>
 function fsalir(){
