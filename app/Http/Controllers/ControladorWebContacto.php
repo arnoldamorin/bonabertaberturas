8fb9c91731e01cae8a11ac6c516bff1d195bb7f0
israShort <israshort@gmail.com>
2020-11-24 21:23:05 -0300

    cambios

--- app/Http/Controllers/ControladorWebContacto.php
+++ app/Http/Controllers/ControladorWebContacto.php
@@ -2,22 +2,11 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-use App\Entidades\Sistema\Patente;
-use App\Entidades\Sistema\Usuario;
-
-require app_path() . '/start/constants.php';
-
-class ControladorWebHome extends Controller
-{
-    public function contacto()
-=======
 require app_path() . '/start/constants.php';
 
 class ControladorWebContacto extends Controller
 {
     public function index()
->>>>>>> e91ecd939ab600e3bea7e4e8ad9aeea905ccd5e4
     {
         return view('web.contacto');
     }
