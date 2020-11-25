8730e9bcb8bbbde44412564b26565f21e3966a98
mauro-arias <mauroacharras@hotmail.com>
2020-11-25 19:16:45 -0300

    cambios varios diseño + arreglos

--- resources/views/web/equipo.blade.php
+++ resources/views/web/equipo.blade.php
@@ -2,38 +2,11 @@
 
 @section('titulo', "Listado de testimonios")
 
-@section('scripts')
-
-  <!-- =======================================================
-  * Template Name: Mentor - v2.1.0
-  * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-
-
-  <!-- ======= Header ======= -->
-
-  <!-- End Header -->
-
-    <!-- ======= Breadcrumbs ======= -->
-    @endsection
 @section('contenido')
-    <!-- ======= Trainers Section ======= -->
-    <section id="trainers" class="trainers">
+    <section id="trainers" class="trainers mt-5">
       <div class="container" data-aos="fade-up">
-
+        <h3>Test</h3>
       </div>
-    </section><!-- End Trainers Section -->
+    </section>
     
-
-<!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <!-- End Footer -->
-
-  <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
-  <div id="preloader"></div>
-
-  <!-- Vendor JS Files -->
-  @endsection
+@endsection
