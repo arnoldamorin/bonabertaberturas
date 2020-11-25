334cc585bb620e07821793267038718ad7d8f015
kevinfigueredo00 <themrpollo111@gmail.com>
2020-11-24 21:09:32 -0300

    cambios

--- app/Http/Controllers/ControladorWebCoachingEjecutivo.php
+++ app/Http/Controllers/ControladorWebCoachingEjecutivo.php
@@ -11,7 +11,7 @@ class ControladorWebCoachingEjecutivo extends Controller
 {
     public function index()
     {
-        return view('web.coachingejecutivo');
+        return view('web.ejecutivo');
     }
 
 }
--- app/Http/Controllers/ControladorWebCoachingEquipo.php
+++ app/Http/Controllers/ControladorWebCoachingEquipo.php
@@ -11,7 +11,7 @@ class ControladorWebCoachingEquipo extends Controller
 {
     public function index()
     {
-        return view('web.coachingequipo');
+        return view('web.equipo');
     }
 
 }
