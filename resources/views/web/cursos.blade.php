7e5196e892c94b733ba89491014344b3c0b9e743
israShort <70781093+israShort@users.noreply.github.com>
2020-11-25 08:17:11 -0300

    cambios en la identación

--- resources/views/web/cursos.blade.php
+++ resources/views/web/cursos.blade.php
@@ -3,92 +3,100 @@
 @section('titulo', "Cursos")
 
 @section('contenido')
-<body>
-  <!-- ======= Breadcrumbs ======= -->
-  <div class="breadcrumbs">
-    <div class="container">
-      <h2>Cursos</h2>
-      <p>Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam aperiam consequatur laboriosam nemo harum praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab officiis, facilis ad quas laudantium quae praesentium accusantium, doloremque corrupti in tempore! Eaque saepe non nihil veritatis nisi fuga quidem minima!</p>
+
+<!-- ======= Breadcrumbs ======= -->
+<div class="breadcrumbs">
+  <div class="container">
+    <h2>Cursos</h2>
+    <p>Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam aperiam consequatur laboriosam nemo harum praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab officiis, facilis ad quas laudantium quae praesentium accusantium, doloremque corrupti in tempore! Eaque saepe non nihil veritatis nisi fuga quidem minima!</p>
+  </div>
+</div><!-- End Breadcrumbs -->
+
+<!-- ======= Courses Section ======= -->
+<section id="courses" class="courses">
+  <div class="container" data-aos="fade-up">
+
+    <div class="row" data-aos="zoom-in" data-aos-delay="100">
+
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3">
+        <!-- Course Item -->
+        <div class="course-item">
+          <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
+          <div class="course-content">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <a href="">
+                <h4>Comprar curso</h4>
+              </a>
+              <p class="price">$17.000</p>
+            </div>
+            <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
+            <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
+            <p><strong>Horario:</strong> A definir con el alumno<br>
+              <strong>Modalidad:</strong> En vivo</p>
+          </div>
+        </div>
+      </div> <!-- End Course Item-->
+
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3">
+        <!-- Course Item -->
+        <div class="course-item">
+          <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
+          <div class="course-content">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <a href="">
+                <h4>Comprar curso</h4>
+              </a>
+              <p class="price">$17.000</p>
+            </div>
+            <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
+            <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
+            <p><strong>Horario:</strong> A definir con el alumno<br>
+              <strong>Modalidad:</strong> En vivo</p>
+          </div>
+        </div>
+      </div> <!-- End Course Item-->
+
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3">
+        <!-- Course Item -->
+        <div class="course-item">
+          <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
+          <div class="course-content">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <a href="">
+                <h4>Comprar curso</h4>
+              </a>
+              <p class="price">$17.000</p>
+            </div>
+            <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
+            <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
+            <p><strong>Horario:</strong> A definir con el alumno<br>
+              <strong>Modalidad:</strong> En vivo</p>
+          </div>
+        </div>
+      </div> <!-- End Course Item-->
+
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3">
+        <!-- Course Item -->
+        <div class="course-item">
+          <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
+          <div class="course-content">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <a href="">
+                <h4>Comprar curso</h4>
+              </a>
+              <p class="price">$17.000</p>
+            </div>
+            <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
+            <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
+            <p><strong>Horario:</strong> A definir con el alumno<br>
+              <strong>Modalidad:</strong> En vivo</p>
+          </div>
+        </div>
+      </div> <!-- End Course Item-->
+
     </div>
-  </div><!-- End Breadcrumbs -->
 
-  <!-- ======= Courses Section ======= -->
-  <section id="courses" class="courses">
-    <div class="container" data-aos="fade-up">
-
-      <div class="row" data-aos="zoom-in" data-aos-delay="100">
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3"> <!-- Course Item -->
-          <div class="course-item">
-            <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
-            <div class="course-content">
-              <div class="d-flex justify-content-between align-items-center mb-3">
-                <a href=""><h4>Comprar curso</h4></a>
-                <p class="price">$17.000</p>
-              </div>
-              <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
-              <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
-              <p><strong>Horario:</strong> A definir con el alumno<br>
-              <strong>Modalidad:</strong> En vivo</p>
-            </div>
-          </div>
-        </div> <!-- End Course Item-->
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3"> <!-- Course Item -->
-          <div class="course-item">
-            <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
-            <div class="course-content">
-              <div class="d-flex justify-content-between align-items-center mb-3">
-                <a href=""><h4>Comprar curso</h4></a>
-                <p class="price">$17.000</p>
-              </div>
-              <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
-              <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
-              <p><strong>Horario:</strong> A definir con el alumno<br>
-              <strong>Modalidad:</strong> En vivo</p>
-            </div>
-          </div>
-        </div> <!-- End Course Item-->
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3"> <!-- Course Item -->
-          <div class="course-item">
-            <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
-            <div class="course-content">
-              <div class="d-flex justify-content-between align-items-center mb-3">
-                <a href=""><h4>Comprar curso</h4></a>
-                <p class="price">$17.000</p>
-              </div>
-              <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
-              <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
-              <p><strong>Horario:</strong> A definir con el alumno<br>
-              <strong>Modalidad:</strong> En vivo</p>
-            </div>
-          </div>
-        </div> <!-- End Course Item-->
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-3"> <!-- Course Item -->
-          <div class="course-item">
-            <img src="../web/img/course-1.jpg" class="img-fluid text-center" alt="...">
-            <div class="course-content">
-              <div class="d-flex justify-content-between align-items-center mb-3">
-                <a href=""><h4>Comprar curso</h4></a>
-                <p class="price">$17.000</p>
-              </div>
-              <h3><a href="course-details.html">Formación Integral en Coaching Deportivo</a></h3>
-              <p>En busca de la excelencia.<br>Brindar diferentes técnicas, procesos y metodologías de Coaching Deportivo que permitan el logro de resultados óptimos y/o extraordinarios.</p>
-              <p><strong>Horario:</strong> A definir con el alumno<br>
-              <strong>Modalidad:</strong> En vivo</p>
-            </div>
-          </div>
-        </div> <!-- End Course Item-->
-
-      </div>
-
-    </div>
-  </section><!-- End Courses Section -->
-
-</body>
+  </div>
+</section><!-- End Courses Section -->
 
 @endsection
-
-</html>
--- resources/views/web/plantilla.blade.php
+++ resources/views/web/plantilla.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -22,140 +23,141 @@
   <link href="{{ asset('web/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
   <link href="{{ asset('web/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
   <link href="{{ asset('web/vendor/aos/aos.css') }}" rel="stylesheet">
-  
+
   <link href="{{ asset('web/css/style.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top">
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-        <h1 class="logo mr-auto"><a href="/">Emilce Charras</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      <h1 class="logo mr-auto"><a href="/">Emilce Charras</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-        <nav class="nav-menu d-none d-lg-block">
+      <nav class="nav-menu d-none d-lg-block">
         <ul>
-            <li class="active"><a href="/">Inicio</a></li>
-            <li><a href="/sobre-mi">Sobre Mi</a></li>
-            <li><a href="/cursos">Cursos</a></li>
-            <li><a href="/conferencias">Conferencias</a></li>
-            <li><a href="/testimonios">Testimonios</a></li>
-            <li class="drop-down"><a href="">Coaching</a>
+          <li class="active"><a href="/">Inicio</a></li>
+          <li><a href="/sobre-mi">Sobre Mi</a></li>
+          <li><a href="/cursos">Cursos</a></li>
+          <li><a href="/conferencias">Conferencias</a></li>
+          <li><a href="/testimonios">Testimonios</a></li>
+          <li class="drop-down"><a href="">Coaching</a>
             <ul>
-                <li><a href="/coaching/sesion-personal">Sesión Personal</a></li>
-                <li><a href="/coaching/equipo">De Equipos</a></li>
-                <li><a href="/coaching/empresarial">Empresarial</a></li>
-                <li><a href="/coaching/deportivo">Deportivo</a></li>
-                <li><a href="/coaching/ejecutivo">Ejecutivo</a></li>
+              <li><a href="/coaching/sesion-personal">Sesión Personal</a></li>
+              <li><a href="/coaching/equipo">De Equipos</a></li>
+              <li><a href="/coaching/empresarial">Empresarial</a></li>
+              <li><a href="/coaching/deportivo">Deportivo</a></li>
+              <li><a href="/coaching/ejecutivo">Ejecutivo</a></li>
             </ul>
-            </li>
-            <li><a href="/contacto">Contacto</a></li>
+          </li>
+          <li><a href="/contacto">Contacto</a></li>
 
         </ul>
-        </nav><!-- .nav-menu -->
+      </nav><!-- .nav-menu -->
 
-        <a href="/cursos" class="get-started-btn">Get Started</a>
+      <a href="/cursos" class="get-started-btn">Get Started</a>
 
     </div>
-    </header><!-- End Header -->
-  
-    <main id="main" data-aos="fade-in">
-        @yield('contenido')
-    </main>
+  </header><!-- End Header -->
 
-<!-- ======= Footer ======= -->
-<footer id="footer">
+  <main id="main" data-aos="fade-in">
+    @yield('contenido')
+  </main>
 
-  <div class="footer-top">
-    <div class="container">
-      <div class="row">
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
 
-        <div class="col-lg-3 col-md-6 footer-contact">
-          <h3>Mentor</h3>
-          <p>
-            A108 Adam Street <br>
-            New York, NY 535022<br>
-            United States <br><br>
-            <strong>Phone:</strong> +1 5589 55488 55<br>
-            <strong>Email:</strong> info@example.com<br>
-          </p>
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Mentor</h3>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Join Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
         </div>
-
-        <div class="col-lg-2 col-md-6 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-3 col-md-6 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-4 col-md-6 footer-newsletter">
-          <h4>Join Our Newsletter</h4>
-          <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-          <form action="" method="post">
-            <input type="email" name="email"><input type="submit" value="Subscribe">
-          </form>
-        </div>
-
       </div>
     </div>
-  </div>
 
-  <div class="container d-md-flex py-4">
+    <div class="container d-md-flex py-4">
 
-    <div class="mr-md-auto text-center text-md-left">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Mentor</span></strong>. All Rights Reserved
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Mentor</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
       </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
-    <div class="social-links text-center text-md-right pt-3 pt-md-0">
-      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-      <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-      <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-    </div>
-  </div>
-</footer><!-- End Footer -->
+  </footer><!-- End Footer -->
 
-<a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
-<div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+  <div id="preloader"></div>
 
-<!-- Vendor JS Files -->
-<script src="{{ asset('web/vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('web/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('web/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
-<script src="{{ asset('web/vendor/php-email-form/validate.js') }}"></script>
-<script src="{{ asset('web/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
-<script src="{{ asset('web/vendor/counterup/counterup.min.js') }}"></script>
-<script src="{{ asset('web/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('web/vendor/aos/aos.js') }}"></script>
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('web/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('web/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('web/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('web/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('web/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ asset('web/vendor/counterup/counterup.min.js') }}"></script>
+  <script src="{{ asset('web/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('web/vendor/aos/aos.js') }}"></script>
 
-<!-- Template Main JS File -->
-<script src="{{ asset('web/js/main.js') }}"></script>
+  <!-- Template Main JS File -->
+  <script src="{{ asset('web/js/main.js') }}"></script>
 
 </body>
+
 </html>
