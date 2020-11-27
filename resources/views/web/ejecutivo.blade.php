f81a255c8246bce74970f76e6ec5ac085567e77d
kevinfigueredo00 <themrpollo111@gmail.com>
2020-11-27 06:02:30 -0300

    cambios

--- resources/views/web/ejecutivo.blade.php
+++ resources/views/web/ejecutivo.blade.php
@@ -3,10 +3,25 @@
 @section('titulo', "Coaching Ejecutivo")
 
 @section('contenido')
-    <section id="trainers" class="trainers mt-5">
-      <div class="container" data-aos="fade-up">
-        <h3>Test</h3>
-      </div>
-    </section>
+<div class="breadcrumbs">
+        <div class="container">
+          <h2 class="text-center">Ejecutivo</h2>
+        </div>
+</div>
+<section id="trainers" class="trainers mt-5">
+  <div class="container" data-aos="fade-up">
+      <div class="row">
+        <div class="col-6">
+          <p class="font-italic text-sm-left text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempore, dignissimos dolores nobis mollitia vitae asperiores expedita error numquam, fugit qui velit nemo harum exercitationem sapiente! Dolorum obcaecati eligendi optio.</p>
+          <div class="col-12 pl-0">
+            <a href="https://api.whatsapp.com/send?phone=" target="_blank" title="Whatsapp" class="get-started-btn ml-0">Agendar Sesión</a>
+          </div>
+        </div>
+        <div class="col-12">
+          <img src="" alt="">
+        </div>
+      </div> 
+  </div>
+</section>
     
 @endsection
--- resources/views/web/equipo.blade.php
+++ resources/views/web/equipo.blade.php
@@ -3,10 +3,25 @@
 @section('titulo', "Coaching De Equipos")
 
 @section('contenido')
-    <section id="trainers" class="trainers mt-5">
-      <div class="container" data-aos="fade-up">
-        <h3>Test</h3>
+<div class="breadcrumbs">
+        <div class="container">
+          <h2 class="text-center">De equipo</h2>
+        </div>
+</div>
+<section id="trainers" class="trainers mt-5">
+  <div class="container" data-aos="fade-up"> 
+    <div class="row">
+      <div class="col-6">
+        <p class="font-italic text-sm-left text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempore, dignissimos dolores nobis mollitia vitae asperiores expedita error numquam, fugit qui velit nemo harum exercitationem sapiente! Dolorum obcaecati eligendi optio.</p>
+        <div class="col-12 pl-0">
+          <a href="https://api.whatsapp.com/send?phone=" target="_blank" title="Whatsapp" class="get-started-btn ml-0">Agendar entrevista</a>
+        </div>
       </div>
-    </section>
+      <div class="col-12">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div> 
+</section>
     
 @endsection
