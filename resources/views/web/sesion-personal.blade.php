f0191e9aa4c3ae762c3fcd5be366f988ecf4c5dd
unknown <jhidalgomez@gmail.com>
2020-11-27 03:01:09 -0300

    .

--- resources/views/web/sesion-personal.blade.php
+++ resources/views/web/sesion-personal.blade.php
@@ -32,37 +32,40 @@
 </head>
 
 <body>
-<!-- ======= Breadcrumbs ======= -->
-<div class="breadcrumbs">
+  <!-- ======= Breadcrumbs ======= -->
+  <div class="breadcrumbs">
     <div class="container">
       <h2>Sesión Personal</h2>
     </div>
   </div><!-- End Breadcrumbs -->
 
- <!-- ======= Section ======= -->
-    <section id="coaching" class="coaching">
-      <div class="container" data-aos="fade-up">
+  <!-- ======= Section ======= -->
+  <section id="coaching" class="coaching">
+    <div class="container" data-aos="fade-up">
 
-        <div class="row mt-5">
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-            <img src=" {{ asset('web/img/diferencia-tu-conferencia.jpg') }}" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-
-            <p class="font-italic text-sm-left text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet a perferendis labore quaerat eaque provident minus accusantium aperiam laboriosam! Obcaecati ad autem error eveniet cumque voluptates odit aliquam optio amet..</p>
-
-          </div>
+      <div class="row mt-5">
+        <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+          <img src=" {{ asset('web/img/diferencia-tu-conferencia.jpg') }}" class="img-fluid" alt="">
         </div>
-        <div clas="row">
+        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+
+          <p class="font-italic text-sm-left text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet a perferendis labore quaerat eaque provident minus accusantium aperiam laboriosam! Obcaecati ad autem error eveniet cumque voluptates odit aliquam optio amet..</p>
+
           <div class="col-12 col-12 pl-0">
-            <a href="https://api.whatsapp.com/send?phone=" target="_blank" title="Whatsapp" class="get-started-btn">¡Quiero una sesión con vos!</a>
+          <a href="https://api.whatsapp.com/send?phone=" target="_blank" title="Whatsapp" class="get-started-btn">¡Quiero una sesión con vos!</a>
           </div>
 
+
         </div>
+      </div>
+      <div clas="row">
+        
 
       </div>
-    </section>
-    <!-- End Section -->
+
+    </div>
+  </section>
+  <!-- End Section -->
 </body>
 
 </html>
