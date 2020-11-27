3bbecc1bd571359c855397e07b927d7b22be4a56
mauro-arias <mauroacharras@hotmail.com>
2020-11-27 19:33:44 -0300

    cambios formulario de compra

--- resources/views/web/detalle-curso.blade.php
+++ resources/views/web/detalle-curso.blade.php
@@ -52,13 +52,16 @@
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <input type="text" name = "txtNombreComprador" id = "txtNombreComprador" placeholder = "Nombre y apellido">
+            <input type="text" name = "txtNombreComprador" id = "txtNombreComprador" placeholder = "Nombre y apellido" class = "form-control text-center">
           </div>
           <div class="col-12">
-            <input type="text" name = "txtCorreoComprador" id = "txtCorreoComprador" placeholder = "Correo">
+            <input class = "form-control text-center mt-2" type="text" name = "txtCorreoComprador" id = "txtCorreoComprador" placeholder = "Correo">
           </div>
           <div class="col-12">
-            <input type="tel" name = "txtTelefonoComprador" id = "txtTelefonoComprador" placeholder = "Teléfono">
+            <input class = "form-control text-center mt-2" type="tel" name = "txtTelefonoComprador" id = "txtTelefonoComprador" placeholder = "Teléfono">
+          </div>
+          <div class="col-12 text-center mt-3">
+            <a href="#" class = "get-started-btn">Comprar</a>
           </div>
         </div>
       </div>
