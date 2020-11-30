625d73d3b710f82eb554b5daf7ba4979adda6caf
israShort <70781093+israShort@users.noreply.github.com>
2020-11-30 15:26:51 -0300

    testimonios

--- resources/views/web/testimonios.blade.php
+++ resources/views/web/testimonios.blade.php
@@ -19,30 +19,31 @@
 
     <h4 class="text-center mt-1">Testimonios escritos</h4>
     <div class="owl-carousel testimonials-carousel" data-aos="zoom-in" data-aos-delay="100">
-      @for ($i = 0; $i < count($aTestimoniosEscritos); $i++) <div class="testimonial-wrap">
-        <div class="testimonial-item">
-          <h3> {{ $aTestimoniosEscritos[$i]->nombre }}</h3>
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-            {{ $aTestimoniosEscritos[$i]->descripcion }}
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
+      @for ($i = 0; $i < count($aTestimoniosEscritos); $i++)
+        <div class="testimonial-wrap">
+          <div class="testimonial-item">
+              <h3> {{ $aTestimoniosEscritos[$i]->nombre }}</h3>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                {{ $aTestimoniosEscritos[$i]->descripcion }}
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+          </div>
         </div>
+      @endfor
     </div>
-    @endfor
-  </div>
 
-  <h4 class="mb-3 text-center mt-5">Testimonios grabados</h4>
-  <div class="row">
-    @for ($i = 0; $i < count($aTestimoniosGrabados); $i++) <div class="col">
-      <div class="card">
-        <h5 class="mt-2 text-center">{{ $aTestimoniosGrabados[$i]->nombre }}</h5>
-        <iframe width="100%" height="200" src="{{ $aTestimoniosGrabados[$i]->video}}" frameborder="0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
-  </div>
-  @endfor
-  </div>
-
+    <h4 class="mb-3 text-center mt-5">Testimonios grabados</h4>
+    <div class="owl-carousel testimonials-carousel" data-aos="zoom-in" data-aos-delay="100">
+      @for ($i = 0; $i < count($aTestimoniosGrabados); $i++)
+        <div class="testimonial-wrap">
+          <div class="testimonial-item">
+              <h5 class="mt-2 text-center">{{ $aTestimoniosGrabados[$i]->nombre }}</h5>
+              <iframe width="100%" height="200" src="{{ $aTestimoniosGrabados[$i]->video }}" frameborder="0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      @endfor
+    </div>
   </div>
 </section><!-- End Testimonials Section -->
 @endsection
