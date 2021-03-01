9ede36028457cd961728642f0078703371ecff35
arnoldamorin <aamorin.ar@gmail.com>
2021-03-01 15:04:40 -0300

    updates

--- app/Http/Controllers/ControladorIngreso.php
+++ app/Http/Controllers/ControladorIngreso.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Entidades\IngresoStock;
-use App\Entidades\Producto;
 use App\Entidades\TipoProducto;
 use Illuminate\Http\Request;
 use App\Entidades\Sistema\Usuario;
@@ -47,14 +46,12 @@ class ControladorIngreso extends Controller
 
         for ($i = $inicio; $i < count($aIngreso) && $cont < $registros_por_pagina; $i++) {
             $row = array();
-            $row[] = $aIngreso[$i]->fk_idventa;
             $row[] = $aIngreso[$i]->fk_idtipo_producto;
             $row[] = $aIngreso[$i]->fk_codproducto;
-            $row[] = $aIngreso[$i]->descrprod;
-            $row[] = $aIngreso[$i]->cantidad;
-            $row[] = "$" . number_format($aIngreso[$i]->preciounitario, 2, ",", "."); 
-            $row[] = "$" . number_format($aIngreso[$i]->total, 2, ",", "."); 
-            $row[] = "<a href='/admin/Ingreso/nuevo/".$aIngreso[$i]->idIngreso."'><i class='fas fa-edit'></i></a>";
+            $row[] = $aIngreso[$i]->fk_cantidad;
+            $row[] = $aIngreso[$i]->fecha;
+            $row[] = $aIngreso[$i]->cantidad;     
+            $row[] = "<a href='/admin/Ingreso/nuevo/".$aIngreso[$i]->idingreso."'><i class='fas fa-edit'></i></a>";
             $cont++;
             $data[] = $row;
         }
@@ -73,16 +70,15 @@ class ControladorIngreso extends Controller
         $tipoProducto = new TipoProducto();
         $array_TipoProducto = $tipoProducto->obtenerTodos();
 
-        $titulo = "Nuevo Ingreso";
-        $idVenta = $_GET["idVenta"];
+        $titulo = "Nuevo Ingreso";        
 
         if (Usuario::autenticado() == true) {
-            if (!Patente::autorizarOperacion("IngresoALTA")) {
-                $codigo = "IngresoALTA";
+            if (!Patente::autorizarOperacion("INGRESOSLTA")) {
+                $codigo = "INGRESOALTA";
                 $mensaje = "No tiene pemisos para la operaci&oacute;n.";
                 return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
             } else {
-                return view('ingreso.ingreso-nuevo', compact('titulo', 'array_TipoProducto','idVenta'));
+                return view('ingreso.ingreso-nuevo', compact('titulo', 'array_TipoProducto'));
             }
         } else {
             return redirect('admin/login');
@@ -95,17 +91,17 @@ class ControladorIngreso extends Controller
         $array_TipoProducto = $tipoProducto->obtenerTodos();
         $titulo = "Modificar Ingreso";
         if (Usuario::autenticado() == true) {
-            if (!Patente::autorizarOperacion("IngresoEDITAR")) {
-                $codigo = "IngresoEDITAR";
+            if (!Patente::autorizarOperacion("INGRESOSEDITAR")) {
+                $codigo = "INGRESOEDITAR";
                 $mensaje = "No tiene pemisos para la operaci&oacute;n.";
                 return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
             } else {
-                $Ingreso = new Ingreso();
-                $Ingreso->obtenerPorId($id);
+                $ingreso = new IngresoStock();
+                $ingreso->obtenerPorId($id);
                 $tipoProducto = new TipoProducto();
                 $array_TipoProducto = $tipoProducto->obtenerTodos();
 
-                return view('ingreso.ingreso-nuevo', compact('Ingreso', 'titulo', 'array_TipoProducto'));
+                return view('ingreso.ingreso-nuevo', compact('ingreso', 'titulo', 'array_TipoProducto'));
                 
             }
         } else {
@@ -118,55 +114,55 @@ class ControladorIngreso extends Controller
         try {
             //Define la entidad servicio
             $titulo = "Modificar Ingreso";
-            $Ingreso = new Ingreso();
-            $Ingreso->cargarDesdeRequest($request);
+            $ingreso = new IngresoStock();
+            $ingreso->cargarDesdeRequest($request);
 
             //validaciones
-            if ($Ingreso->fk_codproducto == "") {
+            if ($ingreso->fk_codproducto == "") {
                 $msg["ESTADO"] = MSG_ERROR;
                 $msg["MSG"] = "Complete todos los datos";
             } else {
                 if ($_POST["id"] > 0) {
                     //Es actualizacion
-                    $Ingreso->guardar();
+                    $ingreso->guardar();
 
                     $msg["ESTADO"] = MSG_SUCCESS;
                     $msg["MSG"] = OKINSERT;
                 } else {
                     //Es nuevo
                     $titulo = "Nuevo Ingreso";
-                    $Ingreso->insertar();
+                    $ingreso->insertar();
 
                     $msg["ESTADO"] = MSG_SUCCESS;
                     $msg["MSG"] = OKINSERT;
-                    $tipoProducto = new TipoProducto();
-                    $array_TipoProducto = $tipoProducto->obtenerTodos();
-                    $idVenta =$Ingreso->fk_idventa;
-                }              
-                return view('ingreso.ingreso-nuevo', compact('titulo', 'msg', 'idVenta', 'array_TipoProducto'));
+            
+                    
+                }    
+                $_POST["id"] = $ingreso->idproducto;          
+                return view('ingreso.ingreso-listar', compact('titulo', 'msg'));
             }
         } catch (Exception $e) {
             $msg["ESTADO"] = MSG_ERROR;
             $msg["MSG"] = ERRORINSERT;
         }
 
-        $id = $Ingreso->idIngreso;
-        $entidad = new Ingreso;
+        $id = $ingreso->idingreso;
+        $entidad = new IngresoStock;
         $entidad->obtenerPorId($id);
 
-        return view('ingreso.ingreso-nuevo', compact('msg', 'entidad', 'titulo')) . '?id=' . $entidad->idIngreso;
+        return view('ingreso.ingreso-nuevo', compact('msg', 'entidad', 'titulo')) . '?id=' . $entidad->idingreso;
     }
 
     public function eliminar(Request $request)
     {
         $id = $request->input('id');
         if (Usuario::autenticado() == true) {
-            if (Patente::autorizarOperacion("IngresoBAJA")) {
+            if (Patente::autorizarOperacion("INGRESOSBAJA")) {
 
-                $Ingreso = new Ingreso();
-                $Ingreso->cargarDesdeRequest($request);
+                $ingreso = new IngresoStock();
+                $ingreso->cargarDesdeRequest($request);
 
-                $Ingreso->eliminar();
+                $ingreso->eliminar();
                 $aResultado["err"] = EXIT_SUCCESS; //eliminado correctamente                         
             } else {
                 //$codigo = "ELIMINARIngreso"; no se porque no se usa
@@ -177,45 +173,5 @@ class ControladorIngreso extends Controller
             return redirect('admin/login');
         }
     }
-    public function buscarProductos(Request $request)
-    {
-        $id = $request->input('id');
-        $producto = new Producto;
-        $array_Producto = $producto->obtenerCodigoPorTipo($id);
-        echo json_encode($array_Producto);
-        exit;
-    }
-    public function buscarProducto(Request $request)
-    {
-        $aResultado = array();
-        $id = $request->input('id');
-        $producto = new Producto();
-        $producto->obtenerPorId($id);
-        $aResultado["precio"] = $producto->precio_venta;
-        $aResultado["cantidad"] = $producto->stock;
-        $aResultado["descripcion"] = $producto->descripcion;
-        echo json_encode($aResultado);
-        exit;
-    }
-    public function buscarCodProducto(Request $request)
-    {
-        $descripcion = $request->input('descripcion');
-        $producto = new Producto();
-        $producto->obtenerPorDescr($descripcion);
-        $aResultado["precio"] = $producto->precio_venta;
-        $aResultado["cantidad"] = $producto->stock;
-        $aResultado["descripcion"] = $producto->codigo;
-        $aResultado["idproducto"] = $producto->idproducto;
-        echo json_encode($aResultado);
-        exit;
-    }
-    public function autocompletar(Request $request)
-    {
-        $palabra = $request->input('palabra');
-        $tipoProducto = $request->input('tipoProducto');
-        $producto = new Producto;
-        $array_Producto = $producto->obtenerFiltradoPalabra($palabra, $tipoProducto);
-        echo json_encode($array_Producto);
-        exit;
-    }
+  
 }
